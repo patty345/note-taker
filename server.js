c3afceb2f77286d4583f824419fb63b061652c85
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-02-12 12:46:51 -0600

    added the starter code for express and the server listener

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const express = requre('express');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+
+
+
+
+
+// Server Listener
+app.listen(PORT, () => {
+    console.log(`API server is ready on port ${PORT}`);
+})
