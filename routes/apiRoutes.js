b5d21fd1d6f3954a4aaad72df7050c718d2e6b08
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-02-12 13:06:18 -0600

    added apiroutes for get and post

---
+++ routes/apiRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const saveData = require('../db/saveData');
+
+// GET request
+router.get('/notes', function(req, res) {
+    saveData.retrieveNotes().then(notes => res.json(notes))
+    .catch(err => res.status(500).json(err));
+});
+
+// POST request
+outer.post('/notes', (req, res) => {
+    saveData
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(err => res.status(500).json(err));
+});
+
+
+
+module.exports = router;
