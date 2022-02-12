77ec69e55f49e5039139134e2be0c9e84557e43f
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-02-12 17:14:34 -0600

    fixed bugs on the functionallity of the app

--- server.js
+++ server.js
@@ -1,22 +1,28 @@
+// Dependencies
 const express = require('express');
-const app = express();
 
+// Point Server to the route files
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// Create an express server
+const app = express();
+
+// Set PORT
 const PORT = process.env.PORT || 3001;
 
+// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
+// Parse incoming JSON data
 app.use(express.json());
+
 
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-
-
-// Server Listener
+// Listener
 app.listen(PORT, () => {
-    console.log(`API server is ready on port ${PORT}`);
-})
+    console.log(`API server is ready on port ${PORT}!`);
+});
